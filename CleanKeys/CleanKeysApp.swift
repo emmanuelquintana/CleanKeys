e25fe4c5c1fdf34a9c734f1ce3013811068e6bbb
emmanuelquintana <quintanatorresjoseemmanuel1dm@gmail.com>
2026-06-25 18:16:11 -0600

    Initial Commit

---
+++ CleanKeys/CleanKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanKeysApp.swift
+//  CleanKeys
+//
+//  Created by Jose emmanuel Quintana torres on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CleanKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanKeys/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanKeys
+//
+//  Created by Jose emmanuel Quintana torres on 25/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
